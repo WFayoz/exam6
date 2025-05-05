36d63c8260f8f190190c530e9bb5ba0b636f2c09
wfayoz <wfayoz@yahoo.com>
2025-05-05 12:19:35 +0500

    changes saved in model

--- db/models.py
+++ db/models.py
@@ -12,19 +12,4 @@ class User(CreatedModel):
     username: Mapped[str] = mapped_column(nullable=True)
 
 
-class Category(CreatedModel):
-    __tablename__ = "categories"
-    name: Mapped[str]
-    todos: Mapped[list['ToDo']] = relationship(back_populates="category", lazy='joined')
-    metadata = Base.metadata
-
-
-class ToDo(CreatedModel):
-    dead_line: Mapped[str] = mapped_column(DATE)
-    title: Mapped[str]
-    description: Mapped[str] = mapped_column(Text)
-    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id', ondelete='CASCADE'))
-    category: Mapped['Category'] = relationship(back_populates="todos", lazy='joined')
-
-
 metadata = Base.metadata
